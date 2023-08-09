c418356d4c802ca51baa04085c3d8819a2b69d0f
JagatheeshSproutwings <jagatheesan.p@sproutwings.in>
2023-08-09 15:05:52 +0530

    Uptodate

---
+++ app/Models/NewlocationHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NewlocationHistory extends Model
+{
+    use HasFactory;
+    protected $table = "new_location_history";
+}
