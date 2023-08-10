b19c2ce924e435f7147722a8efc193a54b019d5c
JagatheeshSproutwings <jagatheesan.p@sproutwings.in>
2023-08-10 18:16:24 +0530

    Add Trigger

---
+++ database/migrations/2023_08_10_110740_add_live_update_trigger.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE TRIGGER live_data_update AFTER INSERT ON `new_location_history` FOR EACH ROW
+        BEGIN
+        INSERT INTO test (device_imei) VALUES(NEW.deviceimei);
+        END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
