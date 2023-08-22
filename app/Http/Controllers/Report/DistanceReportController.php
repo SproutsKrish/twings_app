e4a2e263aea98969193be57f796c6cd1b0e2f40a
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-08-22 13:48:48 +0530

    Distance reports correction

--- app/Http/Controllers/Report/DistanceReportController.php
+++ app/Http/Controllers/Report/DistanceReportController.php
@@ -12,30 +12,38 @@ class DistanceReportController extends Controller
 {
     public function get_distance_report(Request $request)
     {
+        $startDay = $request->input('start_day');
+        $endDay = $request->input('end_day');
+        $deviceImei = $request->input('device_imei');
 
         $results = DB::select("
         SELECT
-            date,
-            min_datetime as start_date,
-            (SELECT latitude FROM play_back_histories WHERE device_datetime = min_datetime) AS start_latitude,
-            (SELECT longitude FROM play_back_histories WHERE device_datetime = min_datetime) AS start_longitude,
-            min_odometer as start_odometer,
-            max_datetime as end_date,
-            (SELECT latitude FROM play_back_histories WHERE device_datetime = max_datetime) AS end_latitude,
-            (SELECT longitude FROM play_back_histories WHERE device_datetime = max_datetime) AS end_longitude,
-            max_odometer as end_odometer,
-            max_odometer - min_odometer AS odometer_difference
-        FROM (
-            SELECT
-                DATE(device_datetime) AS date,
-                MAX(device_datetime) AS max_datetime,
-                MAX(odometer) AS max_odometer,
-                MIN(device_datetime) AS min_datetime,
-                MIN(odometer) AS min_odometer
-            FROM play_back_histories
-            WHERE device_datetime BETWEEN '2023-08-16 06:00:00' AND '2023-08-17 23:59:59' and device_imei = '2109120102295'
-            GROUP BY DATE(device_datetime)
-        ) AS temp;
+        temp.date,
+        temp.min_datetime AS start_date,
+        start_location.latitude AS start_latitude,
+        start_location.longitude AS start_longitude,
+        temp.min_odometer AS start_odometer,
+        temp.max_datetime AS end_date,
+        end_location.latitude AS end_latitude,
+        end_location.longitude AS end_longitude,
+        temp.max_odometer AS end_odometer,
+        temp.max_odometer - temp.min_odometer AS odometer_difference
+    FROM (
+        SELECT
+            DATE(device_datetime) AS date,
+            MAX(device_datetime) AS max_datetime,
+            MAX(odometer) AS max_odometer,
+            MIN(device_datetime) AS min_datetime,
+            MIN(odometer) AS min_odometer
+        FROM play_back_histories
+        WHERE device_datetime BETWEEN '$startDay' AND '$endDay'
+            AND device_imei = '$deviceImei'
+        GROUP BY DATE(device_datetime)
+    ) AS temp
+    JOIN play_back_histories AS start_location ON start_location.device_datetime = temp.min_datetime
+    JOIN play_back_histories AS end_location ON end_location.device_datetime = temp.max_datetime;
+
+
     ");
 
         // dd($results);
