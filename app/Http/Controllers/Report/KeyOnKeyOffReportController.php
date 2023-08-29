b938b37cb7be9ce8e509722a7c6f2808e678f08a
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-08-30 02:17:14 +0530

    trip

--- app/Http/Controllers/Report/KeyOnKeyOffReportController.php
+++ app/Http/Controllers/Report/KeyOnKeyOffReportController.php
@@ -15,44 +15,62 @@ class KeyOnKeyOffReportController extends Controller
         $endDay = $request->input('end_day');
         $deviceIMEI = $request->input('device_imei');
 
-        $results = DB::table('play_back_histories as t')
-            ->joinSub(function ($query) use ($startDay, $endDay, $deviceIMEI, $request) {
-                $query->from('keyoff_keyon_reports')
-                    ->select('vehicle_id', DB::raw('TIMEDIFF(end_datetime, start_datetime) as duration'), 'start_datetime', 'end_datetime', 'start_latitude', 'start_longitude', 'end_latitude', 'end_longitude')
-                    ->where('start_datetime', '>=', $startDay)
-                    ->where('end_datetime', '<=', $endDay)
-                    ->when($request->input('device_imei') !== 'All', function ($query) use ($deviceIMEI) {
-                        return $query->where('device_imei', '=', $deviceIMEI);
-                    });
-            }, 'dt', function ($join) {
-                $join->on('t.device_datetime', '>=', DB::raw('dt.start_datetime'))
-                    ->on('t.device_datetime', '<=', DB::raw('dt.end_datetime'));
-            })
-            ->join('vehicles as c', 'c.id', '=', 'dt.vehicle_id')
-            ->select(
-                'dt.vehicle_id',
-                DB::raw('MAX(c.vehicle_name) as vehicle_name'),
-                'dt.start_datetime',
-                'dt.start_latitude',
-                'dt.start_longitude',
-                'dt.end_datetime',
-                'dt.end_latitude',
-                'dt.end_longitude',
-                DB::raw('FORMAT(MAX(t.speed), 2) AS max_speed'),
-                DB::raw('FORMAT(AVG(t.speed), 2) AS avg_speed'),
-                DB::raw('FORMAT(MAX(t.odometer) - MIN(t.odometer), 2) AS distance'),
-                'dt.duration'
-            )
-            ->groupBy('dt.vehicle_id', 'dt.duration', 'dt.start_datetime', 'dt.end_datetime', 'dt.start_latitude', 'dt.start_longitude', 'dt.end_latitude', 'dt.end_longitude')
-            ->orderBy('dt.start_datetime')
-            ->get();
+        $result = DB::select("SELECT
+        dt.vehicle_id,
+        c.vehicle_name AS vehicle_name,
+        DATE_ADD(dt.start_datetime, INTERVAL 330 MINUTE) AS start_datetime,
+        dt.start_latitude,
+        dt.start_longitude,
+        DATE_ADD(dt.end_datetime, INTERVAL 330 MINUTE) AS end_datetime,
+        dt.end_latitude,
+        dt.end_longitude,
+        FORMAT(MAX(t.speed), 2) AS max_speed,
+        FORMAT(AVG(t.speed), 2) AS avg_speed,
+        FORMAT(MAX(t.odometer) - MIN(t.odometer), 2) AS distance,
+        dt.duration
+    FROM
+        (
+            SELECT
+                vehicle_id,
+                start_latitude,
+                start_longitude,
+                end_latitude,
+                end_longitude,
+                DATE_ADD(start_datetime, INTERVAL 330 MINUTE) as start_datetime,
+                DATE_ADD(end_datetime, INTERVAL 330 MINUTE) as end_datetime,
+                TIMEDIFF(end_datetime, start_datetime) AS duration
+            FROM
+                keyoff_keyon_reports
+            WHERE
+                DATE_ADD(start_datetime, INTERVAL 330 MINUTE) >= '$startDay'
+                AND DATE_ADD(end_datetime, INTERVAL 330 MINUTE) <= '$endDay'
+                AND device_imei = '$deviceIMEI'
+        ) AS dt
+    JOIN
+        play_back_histories AS t
+        ON DATE_ADD(t.device_datetime, INTERVAL 330 MINUTE) >= dt.start_datetime AND DATE_ADD(t.device_datetime, INTERVAL 330 MINUTE) <= dt.end_datetime
+    JOIN
+        vehicles AS c
+        ON c.id = dt.vehicle_id
+    GROUP BY
+        dt.vehicle_id,
+        dt.duration,
+        dt.start_datetime,
+        dt.end_datetime,
+        dt.start_latitude,
+        dt.start_longitude,
+        dt.end_latitude,
+        dt.end_longitude,
+        c.vehicle_name
+    ORDER BY
+        t.id asc");
 
-        if ($results->isEmpty()) {
+        if (empty($result)) {
             $response = ["success" => false, "message" => 'No KeyOnOff Data Found', "status_code" => 404];
             return response($response, 404);
         }
 
-        $response = ["success" => true, "data" => $results, "status_code" => 200];
+        $response = ["success" => true, "data" => $result, "status_code" => 200];
         return response($response, 200);
     }
 }
