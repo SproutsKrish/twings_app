535bf0129ba852ad008c02a81a1270e981a713d5
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-08-30 15:33:37 +0530

    livedata time update

--- app/Http/Controllers/Report/LiveDataController.php
+++ app/Http/Controllers/Report/LiveDataController.php
@@ -210,7 +210,7 @@ class LiveDataController extends BaseController
         // echo $current_time = Carbon::now();
 
         $live_datas = DB::table('live_data')
-            ->where('device_updatedtime', '>', DB::raw('DATE_SUB(NOW(), INTERVAL 10 MINUTE)'))
+            ->where('device_updatedtime', '<', DB::raw('DATE_SUB(NOW(), INTERVAL 10 MINUTE)'))
             ->get();
 
         foreach ($live_datas as $live_data) {
