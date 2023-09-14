b56c2a76c6b233b2148ad085729b78107e0719dc
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-09-14 20:25:04 +0530

    live

--- app/Http/Controllers/Report/ParkingReportController.php
+++ app/Http/Controllers/Report/ParkingReportController.php
@@ -10,14 +10,16 @@ class ParkingReportController extends Controller
 {
     public function get_parking_report(Request $request)
     {
+        $parking_duration = $request->input('duration');
+
         $parkingReports = DB::table('parking_reports as A')
             ->join('vehicles as B', 'A.device_imei', '=', 'B.device_imei')
-            ->where('parking_duration', '>=', $request->input('duration'))
             ->whereRaw("DATE_ADD(A.start_datetime, INTERVAL 330 MINUTE) >= ?", [$request->input('start_day')])
             ->whereRaw("DATE_ADD(A.end_datetime, INTERVAL 330 MINUTE) <= ?", [$request->input('end_day')])
             ->when($request->input('device_imei') !== 'All', function ($query) use ($request) {
                 return $query->where('A.device_imei', '=', $request->input('device_imei'));
             })
+            ->whereRaw("TIMESTAMPDIFF(SECOND, A.start_datetime, A.end_datetime) >= ?", [$parking_duration * 60]) // Filter by parking duration in seconds
             ->select(
                 'A.id',
                 'A.vehicle_id',
@@ -29,7 +31,6 @@ class ParkingReportController extends Controller
                 DB::raw("DATE_ADD(A.end_datetime, INTERVAL 330 MINUTE) as end_datetime"),
                 DB::raw("TIME_FORMAT(TIMEDIFF(A.end_datetime, A.start_datetime), '%H:%i:%s') as parking_duration")
             )
-            ->having('parking_duration', '>=', $request->input('duration'))
             ->orderBy('A.id', 'desc')
             ->get();
 
