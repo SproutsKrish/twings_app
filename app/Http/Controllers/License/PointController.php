72441ed6d726ddd2d631d2b66537ba2674c85cb3
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-09-17 22:42:25 +0530

    GPSTRAK

--- app/Http/Controllers/License/PointController.php
+++ app/Http/Controllers/License/PointController.php
@@ -86,7 +86,7 @@ class PointController extends BaseController
                             } else {
                                 $maxId += 1;
                             }
-                            $prefix = "SWTLIC";
+                            $prefix = "GPSTRAK";
                             $date = Carbon::now()->format('Y');
                             $license_no = $prefix . $date . $maxId;
                             License::create(
