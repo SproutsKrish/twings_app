5fd78bd5d72f4e56864c804b822aa10c5412b360
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-09-20 13:46:00 +0530

    migration

---
+++ database/migrations/2023_09_20_075234_add_columns_to_vehicles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->date('vehicle_expire_date')->nullable()->after('extend_date');
+            $table->date('vehicle_extend_date')->nullable()->after('vehicle_expire_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            //
+        });
+    }
+};
