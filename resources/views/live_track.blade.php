c60f16e4baa53ecc32805803c21c4a5b52b3f7a4
JagatheeshSproutwings <jagatheesan.p@sproutwings.in>
2023-09-25 16:46:49 +0530

    web portal uptodate

---
+++ resources/views/live_track.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>live Track</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+     <script src="{{asset('js/movingMarker.js')}}"></script>
+     <style>
+        #map { height: 180px; }
+     </style>
+</head>
+<body>
+<div id="map" style="height:500px;width:100%"></div>
+</body>
+<script>
+    // initialize the map on the "map" div with a given center and zoom
+var map = new L.Map('map', {
+  zoom: 6,
+  minZoom: 3,
+});
+
+// create a new tile layer
+var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+layer = new L.TileLayer(tileUrl,
+{
+    attribution: 'Maps © <a href=\"www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors',
+    maxZoom: 18
+});
+
+// add the layer to the map
+map.addLayer(layer);
+
+var parisKievLL = [[48.8567, 2.3508], [50.45, 30.523333]];
+var londonParisRomeBerlinBucarest = [[51.507222, -0.1275], [48.8567, 2.3508],
+[41.9, 12.5], [52.516667, 13.383333], [44.4166,26.1]];
+var londonBrusselFrankfurtAmsterdamLondon = [[51.507222, -0.1275], [50.85, 4.35],
+[50.116667, 8.683333], [52.366667, 4.9], [51.507222, -0.1275]];
+var barcelonePerpignanPauBordeauxMarseilleMonaco = [
+    [41.385064, 2.173403],
+    [42.698611, 2.895556],
+    [43.3017, -0.3686],
+    [44.837912, -0.579541],
+    [43.296346, 5.369889],
+    [43.738418, 7.424616]
+];
+
+
+map.fitBounds(londonParisRomeBerlinBucarest);
+
+//========================================================================
+var marker1 = L.Marker.movingMarker(barcelonePerpignanPauBordeauxMarseilleMonaco, [90000]).addTo(map);
+L.polyline(barcelonePerpignanPauBordeauxMarseilleMonaco).addTo(map);
+marker1.once('click', function () {
+    marker1.start();
+    marker1.closePopup();
+    marker1.unbindPopup();
+    marker1.on('click', function() {
+        if (marker1.isRunning()) {
+            marker1.pause();
+        } else {
+            marker1.start();
+        }
+    });
+    setTimeout(function() {
+        marker1.bindPopup('<b>Click me to pause !</b>').openPopup();
+    }, 2000);
+});
+
+marker1.bindPopup('<b>Click me to start !</b>', {closeOnClick: false});
+marker1.openPopup();
+
+
+
+
+</script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PlaybackContoller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('playback/live_track', [PlaybackContoller::class, 'live_track'])->name('live_track');
+Route::resource('playback',PlaybackContoller::class);
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
