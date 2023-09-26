69562b21340e795397588855d72fb1e8f770e5e8
JagatheeshSproutwings <jagatheesan.p@sproutwings.in>
2023-09-26 17:28:08 +0530

    Uptodate

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\PlaybackContoller;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
