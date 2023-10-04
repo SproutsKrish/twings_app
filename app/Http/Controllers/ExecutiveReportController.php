609f7a1962ed79efa45c64305c72ee2ccd6cfeb9
JagatheeshSproutwings <jagatheesan.p@sproutwings.in>
2023-10-04 16:49:30 +0530

    Uptodate

--- app/Http/Controllers/ExecutiveReportController.php
+++ app/Http/Controllers/ExecutiveReportController.php
@@ -22,9 +22,15 @@ class ExecutiveReportController extends Controller
             $start_date = $request->input('start_day');
             $end_date = $request->input('end_day');
             $device_imei = $request->input('device_imei');
+            // $result = DB::table('play_back_histories')
+            // ->join('vehicles','vehicles.device_imei','=','play_back_histories.device_imei')
+            // ->select('vehicles.vehicle_name',DB::Raw())
+            // ->groupBy()
+            // ->get();
+
             $result = DB::table('play_back_histories')
             ->join('vehicles','vehicles.device_imei','=','play_back_histories.device_imei')
-            ->selectRaw('vehicles.vehicle_name as vehicle_name,DATE(device_datetime) as report_date,MIN(odometer) as start_odometer,MAX(odometer) as end_odometer,ROUND(MAX(odometer)-MIN(odometer),2) as distance,MIN(speed) as min_speed,MAX(speed) as max_speed,ROUND(AVG(speed),2) as avg_speed')->where('play_back_histories.device_datetime','>=',$start_date)->where('play_back_histories.device_datetime','<=',$end_date)->where('play_back_histories.device_imei',$device_imei)->groupBy(DB::raw('DATE(play_back_histories.device_datetime)'),'report_date','vehicles.id')->get();
+            ->select('vehicles.vehicle_name as vehicle_name',DB::Raw('DATE(device_datetime) as report_date,MIN(odometer) as start_odometer,MAX(odometer) as end_odometer,ROUND(MAX(odometer)-MIN(odometer),2) as distance,MIN(speed) as min_speed,MAX(speed) as max_speed,ROUND(AVG(speed),2) as avg_speed'))->where('play_back_histories.device_datetime','>=',$start_date)->where('play_back_histories.device_datetime','<=',$end_date)->where('play_back_histories.device_imei',$device_imei)->groupBy(DB::raw('DATE(play_back_histories.device_datetime)'),'report_date','vehicles.id')->get();
             $response = ["success" => true, "data" => $result, "status_code" => 200];
             return response()->json($response, 200);
             } catch (\Throwable $th) {
