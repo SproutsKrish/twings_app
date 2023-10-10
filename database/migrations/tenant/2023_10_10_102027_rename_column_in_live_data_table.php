1977c6bc5a44e7a3ab8a4d159c838e16c61be9fc
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-10-10 16:06:00 +0530

    LI

--- database/migrations/tenant/2023_10_10_102027_rename_column_in_live_data_table.php
+++ database/migrations/tenant/2023_10_10_102027_rename_column_in_live_data_table.php
@@ -8,14 +8,14 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::table('live_data', function (Blueprint $table) {
+        Schema::table('geofence_reports', function (Blueprint $table) {
             $table->renameColumn('assign_geofence_id', 'assign_georeport_id');
         });
     }
 
     public function down()
     {
-        Schema::table('live_data', function (Blueprint $table) {
+        Schema::table('geofence_reports', function (Blueprint $table) {
             $table->renameColumn('assign_georeport_id', 'assign_geofence_id');
         });
     }
