c9b4be781d8d3ed7b9bf946ce0db41eb4a5a4e32
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-10-25 18:43:13 +0530

    li

--- app/Http/Controllers/Report/LiveDataController.php
+++ app/Http/Controllers/Report/LiveDataController.php
@@ -913,11 +913,11 @@ class LiveDataController extends Controller
                 ELSE NULL
                 END AS vehicle_current_status,
             CASE
-                    WHEN ignition = 0 AND device_updatedtime >= DATE_SUB(NOW(), INTERVAL 10 MINUTE) AND expire_date >= CURDATE() THEN "Parking"
-                    WHEN ignition = 1 AND speed = 0 AND device_updatedtime >= DATE_SUB(NOW(), INTERVAL 10 MINUTE) AND expire_date >= CURDATE() THEN "Idle"
-                    WHEN ignition = 1 AND speed > 1 AND device_updatedtime >= DATE_SUB(NOW(), INTERVAL 10 MINUTE) AND expire_date >= CURDATE() THEN "Moving"
+                    WHEN ignition = 0 AND device_updatedtime >= DATE_SUB(NOW(), INTERVAL 10 MINUTE) THEN "Parking"
+                    WHEN ignition = 1 AND speed = 0 AND device_updatedtime >= DATE_SUB(NOW(), INTERVAL 10 MINUTE) THEN "Idle"
+                    WHEN ignition = 1 AND speed > 1 AND device_updatedtime >= DATE_SUB(NOW(), INTERVAL 10 MINUTE) THEN "Moving"
                     WHEN device_updatedtime IS NULL THEN "No Data"
-                    WHEN device_updatedtime < DATE_SUB(NOW(), INTERVAL 10 MINUTE) AND A.expire_date >= CURDATE() THEN "InActive"
+                    WHEN device_updatedtime < DATE_SUB(NOW(), INTERVAL 10 MINUTE) THEN "InActive"
                     WHEN expire_date < CURDATE() THEN "Expired"
                 ELSE NULL
                 END AS current_status',
