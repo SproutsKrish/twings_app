868d2b2d7091c26042238aac031e87b00780a975
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-11-16 12:00:23 +0530

    executive

--- app/Http/Controllers/Report/ExecutiveReportCheckController.php
+++ app/Http/Controllers/Report/ExecutiveReportCheckController.php
@@ -10,7 +10,56 @@ class ExecutiveReportCheckController extends Controller
 {
     public function executive_report_check_list(Request $request)
     {
-        $data = ExecutiveReportCheck::where('user_id', $request->input('user_id'))->first();
+        $isPro = $request->input('ispro');
+        if ($isPro) {
+            $data = ExecutiveReportCheck::select(
+                'start_odometer',
+                'end_odometer',
+                'distance',
+                'avg_speed',
+                'min_speed',
+                'max_speed',
+                'rpm_milege_per_hour',
+                'mileage_per_hour',
+                'start_fuel',
+                'end_fuel',
+                'fuel_fill_litre',
+                'fuel_dip_litre',
+                'fuel_consumed_litre',
+                'mileage',
+                'start_engine_hour_meter',
+                'end_engine_hour_meter',
+                'total_engine_hour_meter',
+                'parking_duration',
+                'idle_duration',
+                'moving_duration',
+                'trip_duration',
+                'ac_duration',
+                'total_rpm_duration',
+                'total_idle_rpm_duration',
+                'total_normal_rpm_duration',
+                'total_max_rpm_duration',
+                'drum_left_rotation',
+                'drum_right_rotation'
+            )
+                ->where('user_id', $request->input('user_id'))->first();
+        } else {
+            $data = ExecutiveReportCheck::select(
+                'start_odometer',
+                'end_odometer',
+                'distance',
+                'avg_speed',
+                'min_speed',
+                'max_speed',
+                'mileage',
+                'parking_duration',
+                'idle_duration',
+                'moving_duration',
+                'trip_duration',
+                'ac_duration',
+            )
+                ->where('user_id', $request->input('user_id'))->first();
+        }
 
         return response()->json(['success' => true, 'data' => $data]);
     }
