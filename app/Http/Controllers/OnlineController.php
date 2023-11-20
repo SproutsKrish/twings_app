2beed3a8c33e753e15cd07a88bf5c45138cdc86d
Krisnakumar <krisnakumar.d@sproutwings.in>
2023-11-20 16:12:44 +0530

    online

--- app/Http/Controllers/OnlineController.php
+++ app/Http/Controllers/OnlineController.php
@@ -42,22 +42,14 @@ class OnlineController extends Controller
             $data['distributor_id'] = $app_info->distributor_id;
             $data['dealer_id'] = $app_info->dealer_id;
             $data['subdealer_id'] = $app_info->subdealer_id;
-            $data['subdealer_id'] = $app_info->subdealer_id;
-            $data['app_id'] = $app_info->subdealer_id;
-
-            $data['subdealer_id'] = $app_info->subdealer_id;
-
+            $data['app_id'] = $app_info->id;
+            $data['app_package_name'] = $app_info->app_package_name;
 
             $country = Country::find($request->input('country_id'));
             if (!$country) {
                 $response = ["success" => false, "message" => "Country does not exist", "status_code" => 404];
                 return response()->json($response, 404);
             }
-            $data['country_id'] = $country->id;
-            $data['country_name'] = $country->country_name;
-            $data['timezone_name'] = $country->timezone_name;
-            $data['timezone_offset'] = $country->timezone_offset;
-            $data['timezone_minutes'] = $country->timezone_minutes;
 
             $data['name'] = $request->input('name');
             $data['email'] = $request->input('email');
@@ -65,7 +57,6 @@ class OnlineController extends Controller
             $data['password'] = $request->input('password');
             $data['country_id'] = $request->input('country_id');
             $data['address'] = $request->input('address');
-
             $data['ip_address'] = $request->ip();
 
             $result = OnlineUser::create($data);
@@ -92,33 +83,14 @@ class OnlineController extends Controller
             DB::beginTransaction();
 
             $validator = Validator::make($request->all(), [
-                'app_id' => 'required|integer',
                 'vehicle_type_id' => 'required|integer',
                 'vehicle_name' => 'required|max:20',
                 'barcode_no' => 'required|unique:online_vehicles,barcode_no',
-                'email' => 'required|max:50',
             ]);
 
             if ($validator->fails()) {
                 $response = ["success" => false, "message" => $validator->errors(), "status_code" => 403];
                 return response()->json($response, 403);
-            }
-
-            $app_info = AppInfo::find($request->input('app_id'));
-            if (!$app_info) {
-                $response = ["success" => false, "message" => "App not valid", "status_code" => 404];
-                return response()->json($response, 404);
-            }
-
-            $stock = OnlineStock::where('barcode_no', $request->input('barcode_no'))
-                ->where('admin_id',  $app_info->admin_id)
-                ->where('distributor_id',  $app_info->distributor_id)
-                ->where('dealer_id', $app_info->dealer_id)
-                ->where('subdealer_id',  $app_info->subdealer_id)
-                ->first();
-            if (!$stock) {
-                $response = ["success" => false, "message" => "Barcode is Invalid", "status_code" => 404];
-                return response()->json($response, 404);
             }
 
             $user = OnlineUser::where('email', $request->input('email'))->first();
@@ -127,17 +99,27 @@ class OnlineController extends Controller
                 return response()->json($response, 404);
             }
 
+            $stock = OnlineStock::where('barcode_no', $request->input('barcode_no'))
+                ->where('admin_id',  $user->admin_id)
+                ->where('distributor_id',  $user->distributor_id)
+                ->where('dealer_id', $user->dealer_id)
+                ->where('subdealer_id',  $user->subdealer_id)
+                ->first();
+            if (!$stock) {
+                $response = ["success" => false, "message" => "Barcode is Invalid", "status_code" => 404];
+                return response()->json($response, 404);
+            }
+
             $vehicle['online_user_id'] = $user->id;
-            $vehicle['barcode_no'] = $request->input('barcode_no');
             $vehicle['vehicle_type_id'] = $request->input('vehicle_type_id');
             $vehicle['vehicle_name'] = $request->input('vehicle_name');
-            $vehicle['description'] = $request->input('description');
-            $vehicle['app_id'] = $request->input('app_id');
-            $vehicle['app_name'] = $request->input('app_name');
-            $vehicle['admin_id'] = $app_info->admin_id;
-            $vehicle['distributor_id'] = $app_info->distributor_id;
-            $vehicle['dealer_id'] = $app_info->dealer_id;
-            $vehicle['subdealer_id'] = $app_info->subdealer_id;
+            $vehicle['barcode_no'] = $request->input('barcode_no');
+            $vehicle['app_id'] =  $user->app_id;
+            $vehicle['app_package_name'] = $user->app_package_name;
+            $vehicle['admin_id'] = $user->admin_id;
+            $vehicle['distributor_id'] = $user->distributor_id;
+            $vehicle['dealer_id'] = $user->dealer_id;
+            $vehicle['subdealer_id'] = $user->subdealer_id;
             $vehicle['ip_address'] = $request->ip();
 
             $results = OnlineVehicle::create($vehicle);
